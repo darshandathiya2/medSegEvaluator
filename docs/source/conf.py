63df4839bc57f0c6c70dde6f0cfcaf70c54a02a2
Darshan  Dathiya <64348663+darshandathiya2@users.noreply.github.com>
2025-12-02 15:01:01 +0530

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,9 +13,11 @@ release = '0.1.0'
 # -- General configuration ---------------------------------------------------
 
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.napoleon',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.coverage",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.githubpages",
 ]
 
 autodoc_default_options = {
