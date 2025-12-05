ead4018f7c3c2fab354788fa4926c5d6bdd142f6
Darshan  Dathiya <64348663+darshandathiya2@users.noreply.github.com>
2025-12-06 00:26:08 +0530

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -61,7 +61,7 @@ autodoc_default_options = {
 # ------------------------------------------------------------------------------
 autodoc_mock_imports = [
     'tensorflow', 'keras', 'cv2', 'numpy', 'pandas',
-    'nibabel', 'pydicom', 'matplotlib'
+    'nibabel', 'pydicom', 'matplotlib', 'scipy'
 ]
 
 # -- Options for HTML output -------------------------------------------------
